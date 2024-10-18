6ed0eb7b1813758414ecf21450833e79a20e4e4f
MikeKent961 <mk961@kent.ac.uk>
2024-10-18 10:37:47 +0000

    i did it yayyy

--- PartOne.py
+++ PartOne.py
@@ -1,1 +1,14 @@
+def main():
+    camelInput = input("Write camel case variable name here: ")
+    snakeOutput = camelToSnake(camelInput)
+    print(snakeOutput)
 
+
+def camelToSnake(varName):
+    for i in varName:
+        if i.isupper() == True:
+            varName = varName.replace(i, "_" + i.lower())
+
+    return varName
+
+main()
--- PartTwo.py
+++ PartTwo.py
@@ -1,1 +1,51 @@
+machineOn = False
+drinksPrice = [75, 50, 100]
+coinsAccepted = [50, 20, 10, 5]
+drinks = ["Coffee", "Tea", "Hot Chocolate"]
+isCoinAccepted = False
 
+def main():
+    print("Coffee Machine :D")
+    machineOn = True
+    while machineOn == True:
+        payment(chooseDrink())
+        userCont = input("Do you want another one? ")
+        if userCont == "no" or userCont == "No":
+            machineOn = False
+    print("aight bye")      
+        
+    
+
+def chooseDrink():
+    print("Please select number of drink you would like.")
+    for i in drinks:
+        print(str(drinks.index(i)+1) + "." , i)
+    
+    return int(input("Enter here: "))
+    
+
+def payment(choice):
+    userPayed = 0
+    while userPayed < drinksPrice[choice-1]:
+        print("Amount left to pay:" , drinksPrice[choice-1] - userPayed)
+        coinInsert = int(input("Insert coin: "))
+        for i in coinsAccepted:
+            if coinInsert == i:
+                isCoinAccepted = True
+                
+            
+        if isCoinAccepted == False:
+            print("Invalid coin.")
+            print("")
+            continue
+        else:
+            userPayed += coinInsert
+            isCoinAccepted=False
+
+        print("")
+
+    if userPayed > choice-1:
+        print("Your change is:" , userPayed-drinksPrice[choice-1])
+
+
+main()
